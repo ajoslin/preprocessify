bbd344ac18bcffb5a8eb907263ac5be90fe3ba30
Andrew Joslin <andrewtjoslin@gmail.com>
2015-07-19 13:11:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var transformTools = require('browserify-transform-tools');
+var path = require('path')
 var pp = require('preprocess');
 
-module.exports = function(preprocessContext, options) {
+module.exports = function(file, options) {
     options = options || {includeExtensions: [".js"]};
+    var ppContext = require(path.resolve(options.context || 'config.js'))
     return transformTools.makeStringTransform("preprocessify", options,
             function (src, transformOptions, done) {
-                done(null, pp.preprocess(src, preprocessContext, 'js'));
+                done(null, pp.preprocess(src, ppContext, 'js'));
             });
 };
